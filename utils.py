04effbc8557d058805fcd6caeefb68c4d1017427
aisearchG <admin@aisearchglobal.com>
2024-08-29 02:57:09 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,16 +1,23 @@
 import os
-import openai
 import streamlit as st
 from datetime import datetime
 from streamlit.logger import get_logger
 from langchain_openai import ChatOpenAI
-from langchain_community.chat_models import ChatOllama
 from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
 
 logger = get_logger('Langchain-Chatbot')
 
+openai_api_key = "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"
+model = "gpt-4o-mini"
+llm = ChatOpenAI(model_name=model, temperature=0, streaming=True, api_key=openai_api_key)
+
+
 #decorator
 def enable_chat_history(func):
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    for msg in st.session_state["messages"]:
+        st.chat_message(msg["role"]).write(msg["content"])
     if os.environ.get("OPENAI_API_KEY"):
 
         # to clear chat history after swtching chatbot
@@ -45,54 +52,7 @@ def display_msg(msg, author):
     st.session_state.messages.append({"role": author, "content": msg})
     st.chat_message(author).write(msg)
 
-def choose_custom_openai_key():
-    openai_api_key = st.sidebar.text_input(
-        label="OpenAI API Key",
-        type="password",
-        placeholder="sk-...",
-        key="SELECTED_OPENAI_API_KEY"
-        )
-    if not openai_api_key:
-        st.error("Please add your OpenAI API key to continue.")
-        st.info("Obtain your key from this link: https://platform.openai.com/account/api-keys")
-        st.stop()
-
-    model = "gpt-4o-mini"
-    try:
-        client = openai.OpenAI(api_key=openai_api_key)
-        available_models = [{"id": i.id, "created":datetime.fromtimestamp(i.created)} for i in client.models.list() if str(i.id).startswith("gpt")]
-        available_models = sorted(available_models, key=lambda x: x["created"])
-        available_models = [i["id"] for i in available_models]
-
-        model = st.sidebar.selectbox(
-            label="Model",
-            options=available_models,
-            key="SELECTED_OPENAI_MODEL"
-        )
-    except openai.AuthenticationError as e:
-        st.error(e.body["message"])
-        st.stop()
-    except Exception as e:
-        print(e)
-        st.error("Something went wrong. Please try again later.")
-        st.stop()
-    return model, openai_api_key
-
 def configure_llm():
-    available_llms = ["gpt-4o-mini","llama3.1:8b","use your openai api key"]
-    llm_opt = st.sidebar.radio(
-        label="LLM",
-        options=available_llms,
-        key="SELECTED_LLM"
-        )
-
-    if llm_opt == "llama3.1:8b":
-        llm = ChatOllama(model="llama3.1", base_url=st.secrets["OLLAMA_ENDPOINT"])
-    elif llm_opt == "gpt-4o-mini":
-        llm = ChatOpenAI(model_name=llm_opt, temperature=0, streaming=True, api_key=st.secrets["OPENAI_API_KEY"])
-    else:
-        model, openai_api_key = choose_custom_openai_key()
-        llm = ChatOpenAI(model_name=model, temperature=0, streaming=True, api_key=openai_api_key)
     return llm
 
 def print_qa(cls, question, answer):
@@ -107,3 +67,4 @@ def configure_embedding_model():
 def sync_st_session():
     for k, v in st.session_state.items():
         st.session_state[k] = v
+        st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
